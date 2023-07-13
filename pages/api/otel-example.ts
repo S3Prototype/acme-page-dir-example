36c2568dc9a1e28ecfcffc553b9a0c57c4debb45
S3Prototype <liuqahs15@gmail.com>
2023-07-13 10:17:34 -0500

    Add otel example route

---
+++ pages/api/otel-example.ts
@@ -0,0 +1,16 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { trace, context } from "@opentelemetry/api";
+
+export default async function getUser(
+  _request: NextApiRequest,
+  response: NextApiResponse
+) {
+  const tracer = trace.getTracer("your-project-name-tracer");
+  // NOTE: You can replace `your-project-name-tracer` with the actual name of your tracer
+  const span = tracer.startSpan("getUser", undefined, context.active());
+  const ms = Math.floor(Math.random() * 1000);
+  span.setAttribute("sleep", ms);
+  await new Promise((resolve) => setTimeout(resolve, ms));
+  response.status(200).json({ greetings: `Hi there ${ms}!` });
+  span.end();
+}
