1201e4d0902ade658569649524d6d94c68efc40b
S3Prototype <liuqahs15@gmail.com>
2023-07-20 07:54:09 -0500

    Update otel-example route

--- pages/api/otel-example.ts
+++ pages/api/otel-example.ts
@@ -1,6 +1,16 @@
-import { registerOTel } from "@vercel/otel";
+import type { NextApiRequest, NextApiResponse } from "next";
+import { trace, context } from "@opentelemetry/api";
 
-export function register() {
-  registerOTel("your-project-name");
+export default async function getUser(
+  _request: NextApiRequest,
+  response: NextApiResponse
+) {
+  const tracer = trace.getTracer("page-dir-example");
+  // NOTE: You can replace `your-project-name-tracer` with the actual name of your tracer
+  const span = tracer.startSpan("getUser", undefined, context.active());
+  const ms = Math.floor(Math.random() * 1000);
+  span.setAttribute("sleep", ms);
+  await new Promise((resolve) => setTimeout(resolve, ms));
+  response.status(200).json({ greetings: `Hi there ${ms}!` });
+  span.end();
 }
-// NOTE: You can replace `your-project-name` with the actual name of your project
